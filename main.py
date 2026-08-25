1ecdaf3abbfe333d1234fc76f66cfd09b590a3c5
Duc <nghdworks@gmail.com>
2026-08-25 16:09:29 +0700

    Number format

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ if errors:
 
 workbook = xlsxwriter.Workbook('output.xlsx', {'nan_inf_to_errors': True})
 for sheet, df in to_excel.items():
-    df.write_excel(workbook=workbook, worksheet=sheet)
+    df.write_excel(workbook=workbook, worksheet=sheet, column_formats = {cs.numeric():'General'})
 workbook.close()
 
 d1_errors = {}
